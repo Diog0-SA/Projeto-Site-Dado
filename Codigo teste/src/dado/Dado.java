abc4ab7e0b4d46268a1c5184e7e45dad4ac28787
Diogo Silva Almeida <160872108+Diog0-SA@users.noreply.github.com>
2024-08-28 13:42:42 -0300

    Add files via upload
    Finalização do código bruto. Falta a tradução para web

--- Codigo teste/src/dado/Dado.java
+++ Codigo teste/src/dado/Dado.java
@@ -5,67 +5,57 @@ import java.util.Scanner;
 
 public class Dado {
 
-	public static void main(String[] args) {
-		
-		// Variaveis
-		int qtdLado = 2;
-		int dado = 1;
-		int qtdDado = 1;
-		
-		Scanner scanner = new Scanner(System.in); // Abrindo scanner
-		
-		System.out.println("Escolha as opções: ");
-		System.out.println("[1] - Personalizado");
-		for(int i = 4; i <= 36; i += 4) {
-			System.out.println("[ " + i + " ]");
-		} 											// Dando as opções
-		
-		// Pegando resposta com tratamento de erro
-		boolean continua = true;
-		while(continua == true) {
-		System.out.println("Digite sua resposta: ");
-		qtdLado = scanner.nextInt(); 
-		
-		// Varificando se o valor é par
-		if(qtdLado % 2 == 0) {
-			continua = false;
-		}
-		else if(qtdLado == 1) {						// Dado personalizado
-			Scanner dadoP = new Scanner(System.in); // Abrindo scanner
-			System.out.println("Quantos lado você deseja: ");
-			qtdLado = dadoP.nextInt();
-			dadoP.close(); // Fechando scanner
-			continua = false;
-		}
-		else {
-			System.out.println("Erro! Tente de novo");
-		}
-		}
-		
-		// Quantos dados deseja:
-		Scanner scannerDado = new Scanner(System.in); // Abrindo scanner
-		
-		System.out.println("Quantos dado deseja? ");
-		qtdDado = scannerDado.nextInt();
-		
-		scannerDado.close(); // Fechando scanner
-		
-		// Fazendo o sorteio do valor
-		for(int i = 1; i <= qtdDado; i++) {	
-			
-		Random random = new Random();
-		dado = random.nextInt(qtdLado);
-		
-		// Mostrando o resultado
-		System.out.println("--------".repeat(7));
-		System.out.printf("Dado n%d°:" + dado + "\n", i);
-		}
-		
-		scanner.close(); // Fechando scanner
-		
-		// tá dando erro no personalizado.         -arrumar
-		// Colocar a opção de dados com números diferentes. Quando chegar da faculdade eu termino
-		
-		
-}
+    public static int apenasInteiro(Scanner scanner, String mensagem) { // Caso digite um número não inteiro
+        System.out.print(mensagem);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    public static void opcoes(int qtdDado, int qtdLado) { // Mostrando as opções
+        Random random = new Random();
+        for (int i = 1; i <= qtdDado; i++) {
+            int resultado = random.nextInt(qtdLado) + 1; 
+            System.out.println("Dado " + i + ": " + resultado);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int qtdDado = apenasInteiro(scanner, "Quantos dados deseja? ");
+            int qtdLado = 0;
+
+            do {
+                menu();
+                qtdLado = apenasInteiro(scanner, "Digite sua opção: ");
+
+                if (qtdLado == 1) {
+                    qtdLado = apenasInteiro(scanner, "Quantos lados o dado personalizado terá? ");
+                } else if (qtdLado >= 4 && qtdLado <= 36 && qtdLado % 2 == 0) {
+                    // Opção válida
+                }else if(qtdLado == 0){
+                	System.out.println("Fim do programa!");
+                }
+                else {
+                    System.out.println("Opção inválida.");
+                }
+
+                if (qtdLado > 0) {
+                    opcoes(qtdDado, qtdLado);
+                    System.out.println("----".repeat(10));
+                }
+            } while (qtdLado > 0);
+        }
+    }
+
+    private static void menu() {
+        System.out.println("Escolha uma opção:");
+        System.out.println("[1] - Dado personalizado");
+        for (int i = 4; i <= 36; i += 4) {
+            System.out.println("[" + i + "]");
+        }
+        System.out.println("[0] - Para");
+    }
 }
